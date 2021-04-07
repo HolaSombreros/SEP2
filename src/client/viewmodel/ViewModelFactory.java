238bde232dee263a69e852797208b6392ecf4717
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-04-07 12:40:43 +0200

    Added RegisterView

--- src/client/viewmodel/ViewModelFactory.java
+++ src/client/viewmodel/ViewModelFactory.java
@@ -1,4 +1,21 @@
 package client.viewmodel;
 
+import client.model.Model;
+
 public class ViewModelFactory {
+    private RegisterViewModel registerViewModel;
+//    private LoginViewModel loginViewModel;
+    
+    public ViewModelFactory(Model model) {
+        registerViewModel = new RegisterViewModel(model);
+//        loginViewModel = new LoginViewModel(model);
+    }
+    
+    public RegisterViewModel getRegisterViewModel() {
+        return registerViewModel;
+    }
+    
+//    public LoginViewModel getLoginViewModel() {
+//        return loginViewModel;
+//    }
 }
