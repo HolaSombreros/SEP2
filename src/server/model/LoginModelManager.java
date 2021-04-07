acebef520a273c352c53660782dfe0398a3eb509
stefaniatomuta <304173@via.dk>
2021-04-07 16:54:43 +0200

    +

--- src/server/model/LoginModelManager.java
+++ src/server/model/LoginModelManager.java
@@ -1,28 +1,27 @@
 package server.model;
 
 import java.beans.PropertyChangeSupport;
-import java.util.ArrayList;
 
 public class LoginModelManager implements LoginModel
 {
-    private UsersList users;
+    private UsersList usersLoggedIn;
     private PropertyChangeSupport property;
 
     public LoginModelManager(){
         property = new PropertyChangeSupport(this);
-        users = new UsersList();
+        usersLoggedIn = new UsersList();
     }
 
     @Override
     public void login(Patient user)
     {
         ServerMessage message;
-        if(users.getUsersList().contains(user)){
+        if(usersLoggedIn.getUsersList().contains(user)){
             message = new ServerMessage("error", null, null,"Username already exists");
             property.firePropertyChange("error", null, message);
         }
         else{
-            users.addUser(user);
+            usersLoggedIn.addUser(user);
             message = new ServerMessage("login", user.getCpr(), user.getPassword(), "Successfully connected to the server");
             property.firePropertyChange("login",null, message);
         }
@@ -38,6 +37,6 @@ public class LoginModelManager implements LoginModel
     @Override
     public UsersList getOnlineUsers()
     {
-        return users;
+        return usersLoggedIn;
     }
 }
