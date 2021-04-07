8fc9a8cfb46797e9fa96bd6c0bae7a7bbf38dfe1
Sombrero <adriana@grecea.net>
2021-04-07 12:32:53 +0200

    loginview

--- src/client/view/LoginViewController.java
+++ src/client/view/LoginViewController.java
@@ -1,4 +1,16 @@
 package client.view;
 
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class LoginViewController {
+
+    public void register(MouseEvent mouseEvent) {
+    }
+
+    public void login(MouseEvent mouseEvent) {
+    }
 }
