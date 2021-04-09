f2680ed5f1f84337dc1710f4c5d9ca434db8ac45
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-04-09 10:57:55 +0200

    Added AddressTest

--- test/server/model/PatientTest.java
+++ test/server/model/PatientTest.java
@@ -21,20 +21,69 @@ class PatientTest {
     @Test
     void setCprIncludingDash() {
         patient1.setCpr("180594-1234");
-        assertEquals("180594-1234", patient1.getCpr());
+        assertEquals("1805941234", patient1.getCpr());
     }
     
     @Test
     void setCprInvalidLength1() {
-        assertThrows(java.lang.IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             patient1.setCpr("1805941-1234");
         });
     }
     
     @Test
     void setCprInvalidLength2() {
-        assertThrows(java.lang.IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             patient1.setCpr("180594-12345");
+        });
+    }
+    
+    @Test
+    void setCprInvalidLength3() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setCpr("1805941234567");
+        });
+    }
+    
+    @Test
+    void setCprNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setCpr(null);
+        });
+    }
+    
+    @Test
+    void setCprEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setCpr("");
+        });
+    }
+    
+    @Test
+    void setPasswordInvalidLength1() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setPassword("1234");
+        });
+    }
+    
+    @Test
+    void setPasswordInvalidLength2() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setPassword("123456789012345678901234567890");
+        });
+    }
+    
+    @Test
+    void setPasswordNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setPassword(null);
+        });
+    }
+    
+    @Test
+    void setPasswordEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            patient1.setPassword("");
         });
     }
 }
