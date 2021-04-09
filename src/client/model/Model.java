910da527c3ffc93b8833cda6aceac50d1afda2e0
stefaniatomuta <304173@via.dk>
2021-04-09 13:31:09 +0200

    mistake made by stefi

--- src/client/model/Model.java
+++ src/client/model/Model.java
@@ -2,13 +2,12 @@ package client.model;
 
 import server.model.Address;
 import server.model.Patient;
+import server.model.UsersList;
 import util.UnnamedPropertyChangeSubject;
-
-import java.util.ArrayList;
 
 public interface Model extends UnnamedPropertyChangeSubject {
   void register(String cpr, String password, String firstName, String middleName, String lastName, Address address, String phone, String email);
   void login(String cpr, String password);
-  ArrayList<Patient> getPatients();
+  UsersList getPatients();
   Patient getPatientByCpr(String CPR);
 }
--- src/client/model/ModelManager.java
+++ src/client/model/ModelManager.java
@@ -1,44 +1,52 @@
 package client.model;
 
 
-import server.model.Address;
-import server.model.Patient;
+import server.model.*;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.ArrayList;
+
 
 public class ModelManager implements Model
 {
   private PropertyChangeSupport property;
-  private ArrayList<Patient> patients;
+  private UsersList patients;
 
   public ModelManager()
   {
     property = new PropertyChangeSupport(this);
-    patients = new ArrayList<>();
+    patients = new UsersList();
   }
 
   @Override public void register(String cpr, String password, String firstName, String middleName, String lastName, Address address, String phone, String email)
   {
     // TODO: Communicate with server
-    patients.add(new Patient(cpr, password, firstName, middleName, lastName, address, phone, email));
+    patients.getUsersList().add(new Patient(cpr, password, firstName, middleName, lastName, address, phone, email));
     System.out.println("Registered patient!");
   }
   
   @Override
   public void login(String cpr, String password) {
       // TODO: Communicate with server
+      if (cpr == null || password == null) {
+        throw new IllegalArgumentException("Input cpr or password");
+      }
+      else {
+        if(getPatientByCpr(cpr) instanceof Nurse || getPatientByCpr(cpr) instanceof Administrator)
+          System.out.println("Logged in as a Nurse or Administrator!");
+        else
+          System.out.println("Logged in as a Patient!");
+      }
   }
   
   @Override
-  public ArrayList<Patient> getPatients() {
+  public UsersList getPatients() {
     return patients;
   }
 
   @Override
   public Patient getPatientByCpr(String CPR) {
-    for(Patient patient : patients)
+    for(Patient patient : patients.getUsersList())
       if(patient.getCpr().equals(CPR))
         return patient;
     return null;
