2a431dd8dba2112272ac95b122103288ca015dc2
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-04-09 10:48:43 +0200

    CPR test cases

--- test/server/model/PatientTest.java
+++ test/server/model/PatientTest.java
@@ -1,47 +1,40 @@
 package server.model;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PatientTest {
+    private Patient patient1;
     
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
-        Patient patient1 = new Patient("220202-1234", "abcdefgh", "Ib", null, "Nielsen", new Address("Vejlevej", "12A", 8700, "Horsens"), "+45 12 12 3434", "email@address.com")
+        patient1 = new Patient("220202-1234", "abcdefgh", "Ib", null, "Nielsen", new Address("Vejlevej", "12A", 8700, "Horsens"), "+4512123434", "email@address.com");
     }
     
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
     
     @Test
-    void setCpr() {
+    void setCprIncludingDash() {
+        patient1.setCpr("180594-1234");
+        assertEquals("180594-1234", patient1.getCpr());
     }
     
     @Test
-    void setPassword() {
+    void setCprInvalidLength1() {
+        assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            patient1.setCpr("1805941-1234");
+        });
     }
     
     @Test
-    void setFirstName() {
-    }
-    
-    @Test
-    void setMiddleName() {
-    }
-    
-    @Test
-    void setLastName() {
-    }
-    
-    @Test
-    void setAddress() {
-    }
-    
-    @Test
-    void setPhone() {
-    }
-    
-    @Test
-    void setEmail() {
+    void setCprInvalidLength2() {
+        assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            patient1.setCpr("180594-12345");
+        });
     }
 }
