3368ac780b242fea0a6db40d5c4451eaf3e1d311
Sombrero <adriana@grecea.net>
2021-05-05 10:22:36 +0200

    update dbs methods

--- src/server/database/AppointmentManager.java
+++ src/server/database/AppointmentManager.java
@@ -13,125 +13,121 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class AppointmentManager {
-  private UserManager userManager;
-  private PatientManager patientManager;
+    private UserManager userManager;
+    private PatientManager patientManager;
 
-  public AppointmentManager() {
+    public AppointmentManager() {
 
-    userManager = new UserManager();
-    patientManager = new PatientManager();
-  }
-
-  public void addAppointment(Appointment appointment) throws SQLException {
-    try (Connection connection = DatabaseManager.getInstance().getConnection())
-    {
-      PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO appointment (date,time_from,time_to,patient_cpr,nurse_cpr,type,status,result) VALUES (?,?,?,?,?,?,?,?)");
-      insertStatement.setDate(1, Date.valueOf(appointment.getDate()));
-      insertStatement.setTime(2, Time.valueOf(appointment.getTimeInterval().getFrom()));
-      insertStatement.setTime(3, Time.valueOf(appointment.getTimeInterval().getTo()));
-      insertStatement.setString(4, appointment.getPatient().getCpr());
-      insertStatement.setString(5, appointment.getNurse().getCpr());
-      insertStatement.setString(6, appointment.getType().toString());
-      insertStatement.setString(7, appointment.getStatus().toString());
-      if (appointment.getType().equals(Type.TEST))
-      {
-        insertStatement.setString(8, ((TestAppointment) appointment).getResult().toString());
-      }
-      else
-        insertStatement.setString(8, null);
-      insertStatement.executeUpdate();
+        userManager = new UserManager();
+        patientManager = new PatientManager();
     }
-  }
 
-  public AppointmentList getAppointmentsByPatient(Patient patient) throws SQLException
-  {
-    try (Connection connection = DatabaseManager.getInstance().getConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM appointment WHERE patient_cpr=?");
-      statement.setString(1, patient.getCpr());
-      ResultSet resultSet = statement.executeQuery();
-      AppointmentList list = new AppointmentList();
-      while (resultSet.next())
-      {
-        Date dateSQL = resultSet.getDate("date");
-        Time timeFromSQL = resultSet.getTime("time_from");
-        Time timeToSQL = resultSet.getTime("time_to");
-        String patientCpr = resultSet.getString("patient_cpr");
-        String nurseCpr = resultSet.getString("nurse_cpr");
-        String nurseId = resultSet.getString("nurse_id");
-        String typeSQL = resultSet.getString("type");
-        String statusSQL = resultSet.getString("status");
-        String resultSQL = resultSet.getString("result");
-        LocalDate date = LocalDate.of(dateSQL.getYear(), dateSQL.getMonth(), dateSQL.getDay());
-        LocalTime timeFrom = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
-        LocalTime timeTo = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
-        Type type = Type.valueOf(typeSQL);
+    public void addAppointment(Appointment appointment) throws SQLException {
+        try (Connection connection = DatabaseManager.getInstance().getConnection()) {
+            PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO appointment (date,time_from,time_to,patient_cpr,nurse_cpr,type,status,result) VALUES (?,?,?,?,?,?,?,?)");
+            insertStatement.setDate(1, Date.valueOf(appointment.getDate()));
+            insertStatement.setTime(2, Time.valueOf(appointment.getTimeInterval().getFrom()));
+            insertStatement.setTime(3, Time.valueOf(appointment.getTimeInterval().getTo()));
+            insertStatement.setString(4, appointment.getPatient().getCpr());
+            insertStatement.setString(5, appointment.getNurse().getCpr());
+            insertStatement.setString(6, appointment.getType().toString());
+            insertStatement.setString(7, appointment.getStatus().toString());
+            if (appointment.getType().equals(Type.TEST)) {
+                insertStatement.setString(8, ((TestAppointment) appointment).getResult().toString());
+            } else
+                insertStatement.setString(8, null);
+            insertStatement.executeUpdate();
+        }
+    }
+
+    public AppointmentList getAppointmentsByPatient(Patient patient) throws SQLException {
+        try (Connection connection = DatabaseManager.getInstance().getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM appointment WHERE patient_cpr=?");
+            statement.setString(1, patient.getCpr());
+            ResultSet resultSet = statement.executeQuery();
+            AppointmentList list = new AppointmentList();
+            while (resultSet.next()) {
+                Date dateSQL = resultSet.getDate("date");
+                Time timeFromSQL = resultSet.getTime("time_from");
+                Time timeToSQL = resultSet.getTime("time_to");
+                String patientCpr = resultSet.getString("patient_cpr");
+                String nurseCpr = resultSet.getString("nurse_cpr");
+                String nurseId = resultSet.getString("nurse_id");
+                String typeSQL = resultSet.getString("type");
+                String statusSQL = resultSet.getString("status");
+                String resultSQL = resultSet.getString("result");
+                LocalDate date = LocalDate.of(dateSQL.getYear(), dateSQL.getMonth(), dateSQL.getDay());
+                LocalTime timeFrom = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
+                LocalTime timeTo = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
+                Type type = Type.valueOf(typeSQL);
 //        Appointment.Status status = Appointment.Status.valueOf(statusSQL);
-        Nurse nurse = userManager.getNurse(nurseCpr);
-        if (type.equals(Type.TEST))
-        {
-          Result result = Result.valueOf(resultSQL);
-          Appointment appointment = new TestAppointment(date, new TimeInterval(timeFrom, timeTo), type, patient, nurse);
-          ((TestAppointment) appointment).setResult(result);
+                Nurse nurse = userManager.getNurse(nurseCpr);
+                if (type.equals(Type.TEST)) {
+                    Result result = Result.valueOf(resultSQL);
+                    Appointment appointment = new TestAppointment(date, new TimeInterval(timeFrom, timeTo), type, patient, nurse);
+                    ((TestAppointment) appointment).setResult(result);
 //          appointment.setStatus(status);
-          list.add(appointment);
-        }
-        else
-        {
-          Appointment appointment = new VaccineAppointment(date, new TimeInterval(timeFrom, timeTo), type, patient, nurse);
+                    list.add(appointment);
+                } else {
+                    Appointment appointment = new VaccineAppointment(date, new TimeInterval(timeFrom, timeTo), type, patient, nurse);
 //          appointment.setStatus(status);
-          list.add(appointment);
+                    list.add(appointment);
+                }
+            }
+            return list;
         }
-      }
-      return list;
     }
-  }
 
-  public AppointmentList getAppointmentsByNurse(Nurse nurse) throws SQLException
-  {
-    try (Connection connection = DatabaseManager.getInstance().getConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM appointment WHERE nurse_cpr=?");
-      statement.setString(1, nurse.getCpr());
-      ResultSet resultSet = statement.executeQuery();
-      AppointmentList list = new AppointmentList();
-      while (resultSet.next())
-      {
-        Date dateSQL = resultSet.getDate("date");
-        Time timeFromSQL = resultSet.getTime("time_from");
-        Time timeToSQL = resultSet.getTime("time_to");
-        String patientCpr = resultSet.getString("patient_cpr");
-        String nurseCpr = resultSet.getString("nurse_cpr");
-        String nurseId = resultSet.getString("nurse_id");
-        String typeSQL = resultSet.getString("type");
-        String statusSQL = resultSet.getString("status");
-        String resultSQL = resultSet.getString("result");
-        LocalDate date = LocalDate.of(dateSQL.getYear(), dateSQL.getMonth(), dateSQL.getDay());
-        LocalTime timeFrom = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
-        LocalTime timeTo = LocalTime.of(timeToSQL.getHours(), timeToSQL.getMinutes());
-        Type type = Type.valueOf(typeSQL);
+    public AppointmentList getAppointmentsByNurse(Nurse nurse) throws SQLException {
+        try (Connection connection = DatabaseManager.getInstance().getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM appointment WHERE nurse_cpr=?");
+            statement.setString(1, nurse.getCpr());
+            ResultSet resultSet = statement.executeQuery();
+            AppointmentList list = new AppointmentList();
+            while (resultSet.next()) {
+                Date dateSQL = resultSet.getDate("date");
+                Time timeFromSQL = resultSet.getTime("time_from");
+                Time timeToSQL = resultSet.getTime("time_to");
+                String patientCpr = resultSet.getString("patient_cpr");
+                String nurseCpr = resultSet.getString("nurse_cpr");
+                String nurseId = resultSet.getString("nurse_id");
+                String typeSQL = resultSet.getString("type");
+                String statusSQL = resultSet.getString("status");
+                String resultSQL = resultSet.getString("result");
+                LocalDate date = LocalDate.of(dateSQL.getYear(), dateSQL.getMonth(), dateSQL.getDay());
+                LocalTime timeFrom = LocalTime.of(timeFromSQL.getHours(), timeFromSQL.getMinutes());
+                LocalTime timeTo = LocalTime.of(timeToSQL.getHours(), timeToSQL.getMinutes());
+                Type type = Type.valueOf(typeSQL);
 //        String status = Appointment.Status.valueOf(statusSQL);
-        User patient = userManager.getPatient(patientCpr);
-        if (type.equals(Type.TEST))
-        {
-          Result result = Result.valueOf(resultSQL);
-          Appointment appointment = new TestAppointment(date, new TimeInterval(timeFrom, timeTo), type, (Patient)patient, nurse);
-          ((TestAppointment) appointment).setResult(result);
+                User patient = userManager.getPatient(patientCpr);
+                if (type.equals(Type.TEST)) {
+                    Result result = Result.valueOf(resultSQL);
+                    Appointment appointment = new TestAppointment(date, new TimeInterval(timeFrom, timeTo), type, (Patient) patient, nurse);
+                    ((TestAppointment) appointment).setResult(result);
 //          appointment.setStatus(status);
-          list.add(appointment);
-        }
-        else
-        {
-          Appointment appointment = new VaccineAppointment(date, new TimeInterval(timeFrom, timeTo), type, (Patient)patient, nurse);
+                    list.add(appointment);
+                } else {
+                    Appointment appointment = new VaccineAppointment(date, new TimeInterval(timeFrom, timeTo), type, (Patient) patient, nurse);
 //          appointment.setStatus(status);
-          list.add(appointment);
+                    list.add(appointment);
+                }
+            }
+            return list;
         }
-      }
-      return list;
     }
-  }
 
-  //TODO: get appointments by date/time interval
+    //TODO: get appointments by date/time interval
+
+    public AppointmentTimeIntervalList getAppointments() throws SQLException {
+        try (Connection connection = DatabaseManager.getInstance().getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("Select * from appointment");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+
+            }
+        }
+        return null;
+    }
 //  public AppointmentList getAppointmentsByNurseAndStatus(Nurse nurse, Appointment.Status status) throws SQLException
 //  {
 //    AppointmentList list = new AppointmentList();
