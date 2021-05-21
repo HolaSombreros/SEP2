d1c47d1b88256e5526cc19a031bb1ce658d3caf9
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-05-21 11:58:56 +0200

    vlaaaaaaaad

--- src/server/model/domain/appointment/AvailableTimeIntervalList.java
+++ src/server/model/domain/appointment/AvailableTimeIntervalList.java
@@ -13,4 +13,13 @@ public class AvailableTimeIntervalList {
     public void add(AvailableTimeInterval availableTimeInterval) {
         availableTimeIntervals.add(availableTimeInterval);
     }
+    
+    @Override
+    public String toString() {
+        String str = "List: ";
+        for (AvailableTimeInterval a : availableTimeIntervals) {
+            str += "\n\t" + a;
+        }
+        return str;
+    }
 }
