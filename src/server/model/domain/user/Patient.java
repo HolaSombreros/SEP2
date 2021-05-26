cca67c15f00b6b1ed05a682294e239fa9f1890ef
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-05-26 11:45:11 +0200

    Observer Design Pattern diagram

--- src/server/model/domain/user/Patient.java
+++ src/server/model/domain/user/Patient.java
@@ -15,7 +15,6 @@ public class Patient extends User {
     
     public Patient(String cpr, String password, String firstName, String lastName, Address address, String phone, String email, VaccineStatus validForVaccine) {
         this(cpr, password, firstName, null, lastName, address, phone, email, validForVaccine);
-        chat = new Chat();
     }
     
     public VaccineStatus getVaccineStatus() {
