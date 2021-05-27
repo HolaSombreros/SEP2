6a7dd3292890101c3ad16627b5738dffcd2279a8
Sombrero <adriana@grecea.net>
2021-05-27 11:52:24 +0300

    faq

--- src/client/viewmodel/FAQViewModel.java
+++ src/client/viewmodel/FAQViewModel.java
@@ -60,6 +60,7 @@ public class FAQViewModel implements FAQViewModelInterface, LocalListener<Object
             vBox.getChildren().add(accordion);
 
             for (FAQ faq : faqList.getQuestions()) {
+                System.out.println(faq);
                 if (faq.getCategory().equals(category)) {
                     TitledPane titledPane = new TitledPane();
                     titledPane.expandedProperty().addListener((obs, oldVal, newVal) -> {
